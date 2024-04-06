d3b8e03a3c3366d356ab675620a748264a761ea8
Erickson Figueroa <indetrd@gmail.com>
2024-04-05 22:55:39 -0500

    Adding hydra and its config file

---
+++ src/process.py
@@ -0,0 +1,23 @@
+"""
+This is the demo code that uses hydra to access the parameters in under the directory config.
+
+Author: Erickson Figueroa
+"""
+
+import hydra
+from omegaconf import DictConfig
+
+
+@hydra.main(config_path="../config", config_name="config", version_base=None)
+def process_data(config: DictConfig):
+    """Sample Function that uses hydra"""
+
+    # Printing the complete list
+    print(f"Columns used: {config.process.use_columns}")
+
+    # Printing the first value
+    print(f"First column used: {config.process.use_columns[0]}")
+
+
+if __name__ == "__main__":
+    process_data()
